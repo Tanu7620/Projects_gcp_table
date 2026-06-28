4a972f3e3f4e9dbe2e7851262bbf66aef6ce7ce9
Tanu7620 <95122286+Tanu7620@users.noreply.github.com>
2026-06-28 15:12:34 +0530

    Update GCS_TO_TABLE.py

--- GCS_TO_TABLE.py
+++ GCS_TO_TABLE.py
@@ -1,5 +1,4 @@
 from airflow import DAG
-from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.utils.dates import days_ago
 
@@ -8,37 +7,34 @@ default_args = {
 }
 
 with DAG(
-    dag_id="local_to_gcs_to_bigquery",
+    dag_id="gcs_to_bigquery_load",
     start_date=days_ago(1),
     schedule=None,
-    catchup=False,
-    default_args=default_args
+    catchup=False
 ) as dag:
 
-    # Task 1: Upload Local File to GCS
-    upload_file = LocalFilesystemToGCSOperator(
-        task_id="upload_csv_to_gcs",
-        src="/home/airflow/data/customer.csv",   # Change this path
-        dst="customer/customer.csv",
-        bucket="my-demo-bucket"
-    )
+    load_data = GCSToBigQueryOperator(
+        task_id="load_csv_to_bq",
 
-    # Task 2: Load CSV into BigQuery
-    load_to_bq = GCSToBigQueryOperator(
-        task_id="load_to_bigquery",
-        bucket="my-demo-bucket",
+        bucket="your-bucket-name",
+
         source_objects=["customer/customer.csv"],
-        destination_project_dataset_table="my_project.demo_dataset.customer",
+
+        destination_project_dataset_table="de-project.sales.customer",
+
         source_format="CSV",
+
         skip_leading_rows=1,
+
         write_disposition="WRITE_TRUNCATE",
+
         schema_fields=[
-            {"name":"customer_id","type":"INTEGER"},
-            {"name":"customer_name","type":"STRING"},
-            {"name":"city","type":"STRING"},
-            {"name":"age","type":"INTEGER"},
-            {"name":"salary","type":"FLOAT"}
+            {"name": "customer_name", "type": "STRING"},
+            {"name": "customer_id", "type": "INTEGER"},
+            {"name": "quantity", "type": "NUMERIC"},
+            {"name": "salary", "type": "FLOAT"},
+            {"name": "created_at", "type": "TIMESTAMP"}
         ]
     )
 
-    upload_file >> load_to_bq
+    load_data
