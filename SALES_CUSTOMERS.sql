5b5f3b31aa83fd6adf0e402380ced2d1d3e7fe7c
Tanu7620 <95122286+Tanu7620@users.noreply.github.com>
2026-06-28 15:04:52 +0530

    Create SALES_CUSTOMERS.sql

---
+++ SALES_CUSTOMERS.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS demo_customer_data
+(
+   customer_name STRING NOT NULL,
+    customer_id INT64 NOT NULL,
+    quantity NUMERIC,
+    salary FLOAT64,
+    created_at TIMESTAMP
+);
