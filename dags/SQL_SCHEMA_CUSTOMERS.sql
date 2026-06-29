b9bf7105727867d7e903b31b224993c73414283c
Tanu7620 <95122286+Tanu7620@users.noreply.github.com>
2026-06-29 15:05:11 +0530

    Update SQL_SCHEMA_CUSTOMERS.sql

--- dags/SQL_SCHEMA_CUSTOMERS.sql
+++ dags/SQL_SCHEMA_CUSTOMERS.sql
@@ -9,26 +9,11 @@ INSERT INTO `bigquery7620.datasetddl.sales_data`
 )
 
 SELECT
-
     SAFE_CAST(customer_name  AS STRING)    AS customer_name,
-    -- ↑ converts to text safely
-
     SAFE_CAST(customer_id    AS INT64)     AS customer_id,
-    -- ↑ converts to whole number safely
-
     SAFE_CAST(quantity       AS NUMERIC)   AS quantity,
-    -- ↑ converts to decimal number safely
-
     SAFE_CAST(salary         AS FLOAT64)   AS salary,
-    -- ↑ converts to float number safely
-
     SAFE_CAST(created_at     AS TIMESTAMP) AS created_at,
-    -- ↑ converts to date and time safely
-
     CURRENT_TIMESTAMP()                    AS lastingestiontime
-    -- ↑ automatically captures date and time of ingestion
-    -- no need to update separately!
 
-FROM `bigquery7620.datasetddl.staging_sales_data`
--- ↑ this is your SOURCE table where raw data lands first from GCS
--- then this SQL cleans and inserts into your target table
+FROM `bigquery7620.datasetddl.sales_data`
