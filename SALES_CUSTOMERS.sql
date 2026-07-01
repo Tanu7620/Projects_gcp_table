bdadb0d3e3c8f8d02d3d0e83bbe14dfbf5a61970
Tanu7620 <95122286+Tanu7620@users.noreply.github.com>
2026-07-01 17:17:25 +0530

    Delete SALES_CUSTOMERS.sql

--- SALES_CUSTOMERS.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS demo_customer_data
-(
-   customer_name STRING NOT NULL,
-    customer_id INT64 NOT NULL,
-    quantity NUMERIC,
-    salary FLOAT64,
-    created_at TIMESTAMP
-);
